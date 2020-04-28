1031987b14655c0714d471d8c6bc167f5572d3f3
Mariusz Galek <MariuszGalek@users.noreply.github.com>
2020-04-28 13:18:41 +0200

    Add buttons for new tasks

--- js/main.js
+++ js/main.js
@@ -9,16 +9,6 @@ const main = () => {
     prepareDOMEvents();
 };
 
-const addNewTask = () => {  if ($todoInput.value !== '') {
-    $newTask = document.createElement('li');
-    $newTask.textContent = $todoInput.value;
-    $ulList.appendChild($newTask);
-    $todoInput.value = '';
-    $alertInfo.innerText = '';
-    } else {
-        $alertInfo.innerText = "Enter your task first!";
-    };
-};
 
 const prepareDOMElements = () => {
     $todoInput = document.querySelector('.todoInput');
@@ -32,6 +22,39 @@ const prepareDOMEvents = () => {
     $addBtn.addEventListener('click', addNewTask);
 };
 
+const addNewTask = () => {  if ($todoInput.value !== '') {
+    $newTask = document.createElement('li');
+    $newTask.textContent = $todoInput.value;
+    $ulList.appendChild($newTask);
+    $todoInput.value = '';
+    $alertInfo.innerText = '';
+    createToolsArea();
+    } else {
+        $alertInfo.innerText = "Enter your task first!";
+    };
+};
 
+const createToolsArea = () => {
+    const toolsPanel = document.createElement('div');
+    toolsPanel.classList.add('tools');
+    $newTask.appendChild(toolsPanel);
+
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class="fa fa-check"></i>';
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit');
+    editBtn.innerText='EDIT';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fa fa-times">';
+
+    toolsPanel.appendChild(completeBtn);
+    toolsPanel.appendChild(editBtn);
+    toolsPanel.appendChild(deleteBtn);
+
+};
 
 document.addEventListener('DOMContentLoaded', main);
